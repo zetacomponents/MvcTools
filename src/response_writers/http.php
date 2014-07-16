af63585ba2f832d4ca1cf0da0012fcdbd0b4cf00
gggeek <gg@ez.no>
2014-07-16 13:19:40 +0200

    Fix: set proper status text when it is received as header

--- src/response_writers/http.php
+++ src/response_writers/http.php
@@ -9,9 +9,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -89,7 +89,14 @@ class ezcMvcHttpResponseWriter extends ezcMvcResponseWriter
         // write output
         foreach ( $this->headers as $header => $value )
         {
-            header( "$header: $value" );
+            if ( strpos( $header, 'HTTP/1.' ) === 0 )
+            {
+                header( "$header $value" );
+            }
+            else
+            {
+                header( "$header: $value" );
+            }
         }
         // do cookies
         foreach ( $this->response->cookies as $cookie )
